144f32eb1e101850e432b0ee97c420ecdeaf725f
Abhinav Shukla <abhinav24shukla08@gmail.com>
2026-01-02 12:30:43 +0530

    Time: 267 ms (32.76%), Space: 296.4 MB (24.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int n,int i,int prev,vector<vector<int>>&dp)
-    {
-        if(i>=n)
-        {
-            return 0;
-        }
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int nottake=solve(nums,n,i+1,prev,dp);
-        int take=0;
-        if(prev==-1||nums[i]>nums[prev])
-        {
-            take=1+solve(nums,n,i+1,i,dp);
-        }
-        return dp[i][prev+1]=max(take,nottake);
-    }
-    int lengthOfLIS(vector<int>& nums) 
-    {
+    // int solve(vector<int>&nums,int n,int i,int prev,vector<vector<int>>&dp)
+    // {
+    //     if(i>=n)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     int nottake=solve(nums,n,i+1,prev,dp);
+    //     int take=0;
+    //     if(prev==-1||nums[i]>nums[prev])
+    //     {
+    //         take=1+solve(nums,n,i+1,i,dp);
+    //     }
+    //     return dp[i][prev+1]=max(take,nottake);
+    // }
+    int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, n, 0, -1, dp);
+
+        // dp[i][prev+1] = LIS starting from index i
+        // when previous index is prev (-1 to n-1)
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // Base case: dp[n][*] = 0 already
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+
+                int nottake = dp[i + 1][prev + 1];
+
+                int take = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i + 1][i + 1];
+                }
+
+                dp[i][prev + 1] = max(take, nottake);
+            }
+        }
+
+        // This corresponds to solve(0, -1)
+        return dp[0][0];
     }
 };
