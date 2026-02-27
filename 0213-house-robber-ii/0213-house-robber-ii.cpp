6e4c24b3d053f499b50bf2721032095e2bce9404
Abhinav Shukla <abhinav24shukla08@gmail.com>
2026-02-27 12:45:30 +0530

    Time: 0 ms (100%), Space: 10.9 MB (38.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
+private:
+    vector<int> nums;
+    int n;
 public:
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-        dp1[0]=nums[0];
-        dp2[0]=nums[1];
-        int a=findway(0,n-2,nums,dp1);
-        int b=findway(1,n-1,nums,dp2);
-        return max(a,b);
+    int solve(int start, int end, int i, vector<int>& dp) {
+        if(i < start) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = nums[i] + solve(start, end, i - 2, dp);
+        int nottake = solve(start, end, i - 1, dp);
+        return dp[i] = max(take, nottake);
     }
-    int findway(int n,int i,vector<int> &nums,vector<int> &dp)
-    {
-        if(i==n) return nums[i];
-        if(i<n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take=nums[i]+findway(n,i-2,nums,dp);
-        int nottake=findway(n,i-1,nums,dp);
-        return dp[i]=max(take,nottake);
+    int rob(vector<int>& nums) {
+        this->nums = nums;
+        n = nums.size();
+        if(n == 1) return nums[0];
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+        int case1 = solve(0, n-2, n-2, dp1);
+        int case2 = solve(1, n-1, n-1, dp2);
+        return max(case1, case2);
     }
 };
