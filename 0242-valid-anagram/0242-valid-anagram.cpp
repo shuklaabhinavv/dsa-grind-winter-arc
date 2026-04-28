af637e561e45c9e362e7d597c7b25672ac6f9c2c
Abhinav Shukla <abhinav24shukla08@gmail.com>
2026-04-28 23:05:27 +0530

    Time: 0 ms (100%), Space: 9.8 MB (24.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
+        if(s.size() != t.size()) return false;
         unordered_map<char,int>m;
         for(auto x:s)
         {
